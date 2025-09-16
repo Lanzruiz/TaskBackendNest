43d1440065e0069ccef8ce11f88c5396200a706a
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-09-16 23:12:20 +0800

    Implement Update the task and delete the task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,20 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   NotFoundException,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import type { ITask } from './task.model';
 import { CreateTaskDTO } from './create-task.dto';
 import { FindOneParams } from './find-one.params';
+import { UpdateTaskStatusDTO } from './update-task.dto';
 
 @Controller('api/tasks')
 export class TasksController {
@@ -24,16 +29,42 @@ export class TasksController {
   //   }
   @Get('/:id')
   public findOne(@Param() params: FindOneParams): ITask {
-    const task = this.taskService.findOne(params.id);
+    // const task = this.taskService.findOne(params.id);
+    // if (task) {
+    //   return task;
+    // }
+    // throw new NotFoundException();
+    return this.findOneOrFail(params.id);
+  }
+  @Post()
+  public create(@Body() createTaskDto: CreateTaskDTO) {
+    return this.taskService.create(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  public updateTaskStatus(
+    @Param() params: FindOneParams,
+    @Body() body: UpdateTaskStatusDTO,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+    task.status = body.status;
+    return task;
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public deleteTask(@Param() params: FindOneParams): void {
+    const task = this.findOneOrFail(params.id);
+    this.taskService.deleteTask(task.id);
+  }
+
+  private findOneOrFail(id: string): ITask {
+    const task = this.taskService.findOne(id);
 
     if (task) {
       return task;
     }
 
     throw new NotFoundException();
-  }
-  @Post()
-  public create(@Body() createTaskDto: CreateTaskDTO) {
-    return this.taskService.create(createTaskDto);
   }
 }
